1b6946da377c7b6703541e4779325cd2aac030b8
Kirill Batrakov <dl9ceb9@gmail.com>
2026-03-22 18:02:42 +0700

    TTL added in config

--- services/movie/src/app/config/config.py
+++ services/movie/src/app/config/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     KINOPOISK_API_KEY: str = ''
     REDIS_HOST: str = 'redis'
     REDIS_PORT: int = 6379
+    SEARCH_BY_KEYWORD_TTL: int = 24 * 3600
+    MOVIE_ID_TTL: int = 24 * 7 * 3600
 
 
 settings = Settings()
